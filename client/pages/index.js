6abc7cbb613678cc818dec9d6ad5604e93789374
alexsuth <alexandernsutherland@gmail.com>
2023-06-03 12:24:33 -0700

    recent

--- client/pages/index.js
+++ client/pages/index.js
@@ -13,32 +13,32 @@ export default function Home({ homePages }) {
       </Head>
       <img src={homePages.nodes[0].homeImage.sourceUrl} className='object-fill w-full h-full mb-6 max-w-7xl items-center justify-center' />
       <div className="flex items-center justify-center">
-              <a href="https://anastasialotusink.hbportal.co/schedule/641672686f80cc002d62c4fe">
-              <button
-                className="hover:shadow-form content-center rounded-md bg-white py-3 mt-10 px-8 text-base font-semibold text-black outline-none"
-              >
-                  BOOK APPOINTMENT
-              </button>
-                </a>
-            </div>
-            <div className="flex items-center justify-center"> 
-            <a href="/appointments">
-              <button
-                className="hover:shadow-form content-center rounded-md bg-white py-3 mt-10 px-8 text-base font-semibold text-black outline-none"
-              >
-                  SEND MESSAGE
-              </button>
-                </a>
-            </div>
-            <div className="flex items-center justify-center"> 
-            <a href="https://www.instagram.com/anastasialotustats/">
-              <button
-                className="hover:shadow-form content-center rounded-md bg-white py-3 mt-10 px-8 text-base font-semibold text-black outline-none"
-              >
-                  INSTAGRAM
-              </button>
-                </a>
-            </div>
+        <a href="https://anastasialotusink.hbportal.co/schedule/641672686f80cc002d62c4fe">
+          <button
+            className="hover:shadow-form content-center rounded-md bg-white py-3 mt-10 px-8 text-base font-semibold text-black outline-none"
+          >
+            BOOK APPOINTMENT
+          </button>
+        </a>
+      </div>
+      <div className="flex items-center justify-center">
+        <a href="/appointments">
+          <button
+            className="hover:shadow-form content-center rounded-md bg-white py-3 mt-10 px-8 text-base font-semibold text-black outline-none"
+          >
+            SEND MESSAGE
+          </button>
+        </a>
+      </div>
+      <div className="flex items-center justify-center">
+        <a href="https://www.instagram.com/anastasialotustats/">
+          <button
+            className="hover:shadow-form content-center rounded-md bg-white py-3 mt-10 px-8 text-base font-semibold text-black outline-none"
+          >
+            INSTAGRAM
+          </button>
+        </a>
+      </div>
     </div>
   );
 }
